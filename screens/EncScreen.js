ee9d97dfae2ba36507eebab5aa0e5648f39a5e19
Zhiero-juro <qzdgremio@tip.edu.ph>
2022-06-19 05:16:18 -0700

    as

--- screens/EncScreen.js
+++ screens/EncScreen.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity } from 'react-native';
-import Butones from '../styles/button';
-import CryptoJs from 'crypto-js';
-import Feather from 'react-native-vector-icons/Feather';
-import { set } from 'react-native-reanimated';
-
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import Butones from "../styles/button";
+import CryptoJs from "crypto-js";
+import Feather from "react-native-vector-icons/Feather";
 
 const EncScreen = ({ navigation }) => {
-
-  const [checkPass,setCheckPass] = useState(true)
-  const [checkKey, setCheckKey] = useState(true)
+  const [checkPass, setCheckPass] = useState(true);
+  const [checkKey, setCheckKey] = useState(true);
   const [pass, setPass] = useState("");
   const [key, setKey] = useState("");
   const [message, setMessage] = useState("");
@@ -18,100 +22,93 @@ const EncScreen = ({ navigation }) => {
   const [encrypted, setEncrypted] = useState("");
   const [decrypted, setDecrypted] = useState("");
 
-  function showPass (){
-    setCheckPass(!checkPass)
+  function showPass() {
+    setCheckPass(!checkPass);
   }
 
-  function showKey (){
-    setCheckKey(!checkKey)
+  function showKey() {
+    setCheckKey(!checkKey);
   }
 
-  function checkEnc(){
-      if(message == ""){
-        alert("Please make sure to enter a text.")
-        setEncrypted("")    
-      }else if(pass == ""){
-        alert("Please make sure to enter a password.")
-        setEncrypted("")
-      }else if(pass.indexOf(" ") >= 0){
-        alert("You cannot enter a space in password.")
-        setEncrypted("")
-      }else{
-        encryption(message, pass)
-      }
-  }
-
-  function checkDec(){
-    if(ciphertext == ""){
-      alert("Please make sure to enter a cipher or encrypted text.")
-      setDecrypted("")
-    }else if(key == ""){
-      alert("Please make sure to enter a key or password.")
-      setDecrypted("")
-    }else if(key.indexOf(" ") >= 0){
-      alert("You cannot enter a space in password.")
-      setDecrypted("")
-    }else{
-      decryption(ciphertext, key)
+  function checkEnc() {
+    if (message == "") {
+      alert("Please make sure to enter a text.");
+      setEncrypted("");
+    } else if (pass == "") {
+      alert("Please make sure to enter a password.");
+      setEncrypted("");
+    } else if (pass.indexOf(" ") >= 0) {
+      alert("You cannot enter a space in password.");
+      setEncrypted("");
+    } else {
+      encryption(message, pass);
     }
   }
 
+  function checkDec() {
+    if (ciphertext == "") {
+      alert("Please make sure to enter a cipher or encrypted text.");
+      setDecrypted("");
+    } else if (key == "") {
+      alert("Please make sure to enter a key or password.");
+      setDecrypted("");
+    } else if (key.indexOf(" ") >= 0) {
+      alert("You cannot enter a space in password.");
+      setDecrypted("");
+    } else {
+      decryption(ciphertext, key);
+    }
+  }
 
   function encryption(Message, Password) {
     var salt = CryptoJs.lib.WordArray.random(128 / 8);
     var iv = CryptoJs.lib.WordArray.random(128 / 8);
     var key = CryptoJs.PBKDF2(Password, salt, {
-      keySize: 128 / 32
+      keySize: 128 / 32,
     });
 
     var encrypted = CryptoJs.AES.encrypt(Message, key, { iv: iv });
-    var finalEncryption = salt.toString() + iv.toString() + encrypted.toString()
-    console.log(finalEncryption)
-    console.log("==================================")
+    var finalEncryption =
+      salt.toString() + iv.toString() + encrypted.toString();
+    console.log(finalEncryption);
+    console.log("==================================");
     setEncrypted(finalEncryption);
     return finalEncryption;
   }
 
   function decryption(finalEncryption, Password) {
-    
-    var salt = CryptoJs.enc.Hex.parse(finalEncryption.substr(0, 32))
-    var iv = CryptoJs.enc.Hex.parse(finalEncryption.substr(32, 32))
-    var encrypted = finalEncryption.substr(64)
+    var salt = CryptoJs.enc.Hex.parse(finalEncryption.substr(0, 32));
+    var iv = CryptoJs.enc.Hex.parse(finalEncryption.substr(32, 32));
+    var encrypted = finalEncryption.substr(64);
 
     var key = CryptoJs.PBKDF2(Password, salt, {
-      keySize: 128 / 32
+      keySize: 128 / 32,
     });
 
-
     var decrypted = CryptoJs.AES.decrypt(encrypted, key, { iv: iv });
-    console.log(decrypted)
+    console.log(decrypted);
     var finalDecrypted = decrypted.toString(CryptoJs.enc.Utf8);
-    console.log("Decypted: " + decrypted.toString(CryptoJs.enc.Utf8))
+    console.log("Decypted: " + decrypted.toString(CryptoJs.enc.Utf8));
     setDecrypted(finalDecrypted);
-    if(finalDecrypted == ""){
-      alert("Wrong password. Please enter the right password")
-      setDecrypted("")
-    }else{
+    if (finalDecrypted == "") {
+      alert("Wrong password. Please enter the right password");
+      setDecrypted("");
+    } else {
       return decrypted;
     }
   }
 
-
   return (
     <ScrollView style={styles.scroller}>
-      <View style={styles.container} >
+      <View style={styles.container}>
         <StatusBar style="auto" />
 
         <View style={styles.card}>
-          <Text style={styles.userName} >Encryption</Text>
+          <Text style={styles.userName}>Encryption</Text>
 
           <View style={styles.action}>
-            <Feather 
-              name="shield"
-              color="#000000"
-              size={23}
-            />
-            
+            <Feather name="shield" color="#000000" size={23} />
+
             <TextInput
               placeholder="Enter message"
               placeholderTextColor="#000000"
@@ -120,21 +117,12 @@ const EncScreen = ({ navigation }) => {
               autoCapitalize="none"
               onChangeText={(val) => setMessage(val)}
             />
-            {message != "" ? 
-            <Feather 
-            name="check-circle"
-            color="#000000"
-            size={23}
-            />  : null
-            }
-            
+            {message != "" ? (
+              <Feather name="check-circle" color="#000000" size={23} />
+            ) : null}
           </View>
           <View style={styles.action}>
-          <Feather 
-              name="lock"
-              color="#000000"
-              size={23}
-            />
+            <Feather name="lock" color="#000000" size={23} />
             <TextInput
               placeholder="Enter password"
               placeholderTextColor="#000000"
@@ -145,42 +133,30 @@ const EncScreen = ({ navigation }) => {
               onChangeText={(val) => setPass(val)}
             />
             <TouchableOpacity onPress={showPass}>
-            {checkPass ? 
-            <Feather 
-            name="eye-off"
-            color="#000000"
-            size={23}
-            /> : 
-            <Feather 
-              name="eye"
-              color="#000000"
-              size={23}
-            />
-            }
+              {checkPass ? (
+                <Feather name="eye-off" color="#000000" size={23} />
+              ) : (
+                <Feather name="eye" color="#000000" size={23} />
+              )}
             </TouchableOpacity>
           </View>
 
-          <Butones
-            text="Encrypt"
-            onPress={() => checkEnc(message, pass)}
-          />
+          <Butones text="Encrypt" onPress={() => checkEnc(message, pass)} />
         </View>
 
         <View style={styles.card}>
-          <Text style={styles.userName} >Ciphertext</Text>
-          <Text style={styles.result} selectable={true} > {encrypted} </Text>
+          <Text style={styles.userName}>Ciphertext</Text>
+          <Text style={styles.result} selectable={true}>
+            {" "}
+            {encrypted}{" "}
+          </Text>
         </View>
 
-
         <View style={styles.card}>
-          <Text style={styles.userName} >Decryption</Text>
+          <Text style={styles.userName}>Decryption</Text>
 
           <View style={styles.action}>
-          <Feather 
-              name="shield"
-              color="#000000"
-              size={23}
-            />
+            <Feather name="shield" color="#000000" size={23} />
             <TextInput
               placeholder="Enter ciphertext"
               placeholderTextColor="#000000"
@@ -189,20 +165,12 @@ const EncScreen = ({ navigation }) => {
               autoCapitalize="none"
               onChangeText={(val) => setCiphertext(val)}
             />
-            {ciphertext != "" ? 
-            <Feather 
-            name="check-circle"
-            color="#000000"
-            size={23}
-            />  : null
-            }
+            {ciphertext != "" ? (
+              <Feather name="check-circle" color="#000000" size={23} />
+            ) : null}
           </View>
           <View style={styles.action}>
-          <Feather 
-              name="lock"
-              color="#000000"
-              size={23}
-            />
+            <Feather name="lock" color="#000000" size={23} />
             <TextInput
               placeholder="Enter password"
               placeholderTextColor="#000000"
@@ -213,37 +181,28 @@ const EncScreen = ({ navigation }) => {
               onChangeText={(val) => setKey(val)}
             />
             <TouchableOpacity onPress={showKey}>
-            {checkKey ? 
-            <Feather 
-            name="eye-off"
-            color="#000000"
-            size={23}
-            /> : 
-            <Feather 
-              name="eye"
-              color="#000000"
-              size={23}
-            />
-            }
+              {checkKey ? (
+                <Feather name="eye-off" color="#000000" size={23} />
+              ) : (
+                <Feather name="eye" color="#000000" size={23} />
+              )}
             </TouchableOpacity>
           </View>
 
-          <Butones
-            text="Decrypt"
-            onPress={() => checkDec(ciphertext, key)}
-          />
+          <Butones text="Decrypt" onPress={() => checkDec(ciphertext, key)} />
         </View>
 
         <View style={styles.card}>
-          <Text style={styles.userName} >Plaintext</Text>
-          <Text style={styles.result} selectable={true} > {decrypted} </Text>
+          <Text style={styles.userName}>Plaintext</Text>
+          <Text style={styles.result} selectable={true}>
+            {" "}
+            {decrypted}{" "}
+          </Text>
         </View>
-
-
       </View>
     </ScrollView>
   );
-}
+};
 
 export default EncScreen;
 
@@ -251,83 +210,80 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
 
-    alignItems: 'center',
+    alignItems: "center",
 
     padding: 20,
-
   },
   scroller: {
     flex: 1,
-    backgroundColor: 'rgba(0, 0, 0, .9)',
+    backgroundColor: "rgba(0, 0, 0, .9)",
   },
   textInput: {
     flex: 1,
     paddingLeft: 8,
-    color: '#000000',
+    color: "#000000",
     fontSize: 12,
-
   },
   action: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 10,
     borderBottomWidth: 1.5,
-    borderBottomColor: '#000000',
+    borderBottomColor: "#000000",
     paddingBottom: 5,
     marginBottom: 10,
-    marginLeft: '8%',
-    marginRight: '8%',
+    marginLeft: "8%",
+    marginRight: "8%",
   },
   card: {
-    backgroundColor: 'rgba(131, 238, 255, 0.8)',
+    backgroundColor: "rgba(131, 238, 255, 0.8)",
     width: "100%",
     marginBottom: 20,
     borderRadius: 10,
     paddingBottom: 8,
-    paddingTop: 8
+    paddingTop: 8,
   },
   userImg: {
     height: 150,
     width: 150,
     borderRadius: 75,
     borderWidth: 2,
-    borderColor: '#83EEFF',
-    marginTop: 25
-
+    borderColor: "#83EEFF",
+    marginTop: 25,
   },
   userName: {
     fontSize: 25,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 0,
     marginBottom: 8,
-    color: 'black',
-    textAlign: 'center',
-    fontFamily: "Roboto"
+    color: "black",
+    textAlign: "center",
+    fontFamily: "Roboto",
   },
   aboutUser: {
     fontSize: 12,
-    textAlign: 'justify',
+    textAlign: "justify",
     marginBottom: 10,
-    color: 'black',
-    marginLeft: '10%',
-    marginRight: '10%'
+    color: "black",
+    marginLeft: "10%",
+    marginRight: "10%",
   },
   result: {
     fontSize: 12,
-    textAlign: 'left',
+    textAlign: "left",
     marginBottom: 10,
-    color: 'black',
-    marginLeft: '10%',
-    marginRight: '10%'
+    color: "black",
+    marginLeft: "10%",
+    marginRight: "10%",
   },
   userBtnWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "center",
+    width: "100%",
     marginBottom: 5,
     marginTop: 10,
   },
   userBtn: {
-    borderColor: 'rgba(255,192,203, .7)',
+    borderColor: "rgba(255,192,203, .7)",
     borderWidth: 3,
     borderRadius: 30,
     paddingVertical: 8,
@@ -335,35 +291,33 @@ const styles = StyleSheet.create({
     marginHorizontal: 5,
   },
   userBtnTxt: {
-    color: '#fff',
+    color: "#fff",
     padding: 5,
     fontSize: 18,
   },
   userInfoWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
     marginVertical: 20,
     borderWidth: 2,
-    borderColor: 'transparent',
+    borderColor: "transparent",
     paddingBottom: 15,
-    borderBottomColor: '#83EEFF'
+    borderBottomColor: "#83EEFF",
   },
   userInfoItem: {
-    justifyContent: 'center',
-
+    justifyContent: "center",
   },
   userInfoTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
-    textAlign: 'center',
-    color: 'rgba(131, 238, 255, 0.8)',
+    textAlign: "center",
+    color: "rgba(131, 238, 255, 0.8)",
   },
   userInfoSubTitle: {
     fontSize: 12,
-    color: '#666',
-    textAlign: 'center',
-
+    color: "#666",
+    textAlign: "center",
   },
 });
